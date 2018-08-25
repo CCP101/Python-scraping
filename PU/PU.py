c448f244b91599b20604326d898f91fe31039ef3
CCP101 <liwentao0523@hotmail.com>
2018-08-25 19:04:22 +0800

    Python Scraping Learning
    完成PU平台数据抓取
    使用cookies成功获取学生学号
    
    Time:2018/8/25
    Location:home

--- PU/PU.py
+++ PU/PU.py
@@ -1,18 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+
 count = 1
 school_student = []
-cookie_str = r"PHPSESSID=36fa74305b7fdeb81395a; Hm_lvt_dd3" \
-             r"ea352543392a029ccf9da1be54a50=1535029961,15351067" \
-             r"43; TS_think_language=zh-CN; TS_LOGGED_USER=ZUdBLN" \
-             r"H%3D8LWr37Zuotakxs5tv; Hm_lpvt_dd3ea352543392a02" \
-             r"9ccf9da1be54a50=1535120447"
-url = ("http://jcnuaa.pocketuni.net/index.php?app=event&mod=School&act=rank&k=3&&p=%d" % count)
-response = requests.get(url)
-for i in range(700):
+cookie_str = "XXXXXXXXXXXXXXXX"
+for i in range(742):
     url = ("http://jcnuaa.pocketuni.net/index.php?app=event&mod=School&act=rank&k=3&&p=%d" % count)
-    response = requests.get(url)
+    cookies = {}
+    for line in cookie_str.split(';'):
+        key, value = line.split('=', 1)
+        cookies[key] = value
+    headers = {'User-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                             "Chrome/68.0.3440.106 Safari/537.36"}
+    response = requests.get(url, headers=headers, cookies=cookies)
     # print(response)
     response.encoding = "utf-8"
     contents = response.text
@@ -24,7 +25,7 @@ for i in range(700):
                        "<tr>\s*"
                        "<td>([0-9]+)</td>\s*"
                        "<td>([\u4e00-\u9fa5]{2,})</td>\s*"
-                       "<td></td>\s*"
+                       "<td>([0-9]{8,})</td>\s*"
                        "<td class=\"rank4\">([1-9]\d*.\d*|0.\d*[1-9]\d*)</td>"
                        , str(List))
     for a in range(10):
@@ -34,9 +35,3 @@ for i in range(700):
 print(school_student)
 # wf = open(r"C:\Users\liwen\Desktop\nanhang.txt", "wb")
 # wf.write(school_student)
-
-
-
-
-
-
